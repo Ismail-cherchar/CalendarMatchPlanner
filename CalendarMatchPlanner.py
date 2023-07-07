44a36ad71b0cef25b17dfab91abdda562d77d7dc
Ismail CHERCHAR <100292900+Ismail-cherchar@users.noreply.github.com>
2023-07-07 16:21:19 +0200

    Update CalendarMatchPlanner.py

--- CalendarMatchPlanner.py
+++ CalendarMatchPlanner.py
@@ -1,29 +1,53 @@
-# Author : Ismail CHERCHAR
-# Name : CalendarMatchPlanner
-# Description : Program that offers a solution for randomly dividing 5 teams into 4 pools and scheduling matches fairly over a specified calendar.
-# Date : 27/06/2023
+from datetime import datetime, timedelta
 
-import random  # Importation du module random 
-from datetime import datetime, timedelta  # importation du module datetime et timedelta
+teams = [chr(k) for k in range(ord("A"), ord("T") + 1)]
 
-teams = [chr(k) for k in range(ord("A"), ord("T")+1)]  # Création d'une liste des équipes de l'alphabet A à T (20 équipes)
-random.shuffle(teams)  # Mélange aléatoire des équipes dans la liste
-
-poules = [teams[i:i+5] for i in range(0, len(teams), 5)]  # Création des poules en regroupant les équipes par groupe de 5
+poules = [teams[i:i+5] for i in range(0, len(teams), 5)]
 
 matches = [[(team1, team2) for i, team1 in enumerate(poule) for team2 in poule[i+1:]] for poule in poules]
-# Création des paires de matchs pour chaque poule en combinant toutes les équipes entre elles
 
-start_date = datetime.now()  # Récupération de la date et de l'heure 
-current_date = start_date  # Initialisation de la date de départ pour les matchs
+start_date = datetime.now()
+current_date = start_date
 
-for i, poule in enumerate(poules):  # Parcours de chaque poule avec l'indice
-    print(f"Poule {i + 1}: {', '.join(poule)}")  # Affichage du numéro de la poule et des équipes 
-    for team in poule:  # Parcours de chaque équipe dans la poule
-        opponents = [opponent for match in matches[i] for opponent in match if team in match and opponent != team]
-        # Récupération des adversaires pour chaque équipe dans la poule
-        print(f"{team} contre :")  # Affichage de l'équipe 
-        for opponent in opponents:  # Parcours de chaque adversaire
-            print(f"  {current_date.strftime('%d-%m-%Y')}: {opponent}")  # Affichage de la date et de l'adversaire
-            current_date += timedelta(days=random.randint(1, 2))  # Incrémentation de la date actuelle avec un délai aléatoire de 1 ou 2 jours
-    print()  # Affichage d'une ligne vide entre chaque poule
+def schedule_match(team, opponent):
+    global current_date
+    print(f"  {current_date.strftime('%d-%m-%Y')}: {opponent}")
+    current_date += timedelta(days=1)
+
+def schedule_last_match(team, opponent):
+    global current_date
+    print(f"  {current_date.strftime('%d-%m-%Y')}: {opponent} ")
+    current_date += timedelta(days=1)
+
+for i, poule in enumerate(poules):
+    print(f"Poule {i + 1}: {', '.join(poule)}")
+    for j, team in enumerate(poule):
+        opponents = [opponent for opponent in poule if opponent != team]
+        print(f"{team} contre :")
+        
+        if team == 'B' and 'A' in opponents:
+            opponents.remove('A')
+            opponents.append('A')
+
+        
+        if team == 'G' and 'F' in opponents:
+            opponents.remove('F')
+            opponents.append('F')
+        
+        if team == 'L' and 'K' in opponents:
+            opponents.remove('K')
+            opponents.append('K')
+        
+        if team == 'Q' and 'P' in opponents:
+            opponents.remove('P')
+            opponents.append('P')
+        
+        for opponent in opponents[:-1]:
+            schedule_match(team, opponent)
+        
+        if team == 'B':
+            schedule_last_match(team, opponents[-1])
+        else:
+            schedule_match(team, opponents[-1])
+    
+    print()
