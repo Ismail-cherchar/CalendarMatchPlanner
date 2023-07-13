2c3fcafce863d874981add8ad6bfea8fce5f774b
Ismail CHERCHAR <100292900+Ismail-cherchar@users.noreply.github.com>
2023-07-13 10:22:30 +0200

    Update CalendarMatchPlanner.py

--- CalendarMatchPlanner.py
+++ CalendarMatchPlanner.py
@@ -11,13 +11,12 @@ for i in range(len(teams)):
 #Variable definition
 new_match = []
 counter = 0
-last_match = None
+last_match = []
 
-#Verification of each element for sublist
 while counter < 10:
     for sublist in match:
         match_string = ''.join(sublist)
-        if last_match is not None and any(element in last_match for element in sublist):
+        if last_match is not [] and any(element in last_match for element in sublist):
             continue
 
         if match_string not in new_match:
@@ -25,12 +24,9 @@ while counter < 10:
             counter += 1
             last_match = sublist
 
-        if counter == 10:
-            break
-
-    if counter == 10: #ten matchs 
+    if counter == 10:
         break
 
-# Afficher new_match
+#print new_match
 for item in new_match:
     print(item)
