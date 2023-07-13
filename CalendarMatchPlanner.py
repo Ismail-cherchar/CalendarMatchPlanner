460008af301e2d622b6175f1bcab1e8bb9c29e83
Ismail CHERCHAR <100292900+Ismail-cherchar@users.noreply.github.com>
2023-07-13 10:46:34 +0200

    Update CalendarMatchPlanner.py

--- CalendarMatchPlanner.py
+++ CalendarMatchPlanner.py
@@ -1,14 +1,12 @@
-teams = ["A", "B", "C", "D", "E"] #Create teams
+teams = ["A", "B", "C", "D", "E"]
 
-match = [] #Create match
+match = []
 
-#Delete duplicates teams
 for i in range(len(teams)):
     for j in range(len(teams)):
         if teams[i] != teams[j] and teams[j] + teams[i] not in match:
             match.append(teams[i] + teams[j])
 
-#Variable definition
 new_match = []
 counter = 0
 last_match = []
@@ -16,16 +14,13 @@ last_match = []
 while counter < 10:
     for sublist in match:
         match_string = ''.join(sublist)
-        if last_match != [] and any(element in last_match for element in sublist):
+        if any(element in last_match for element in sublist):
             continue
 
         if match_string not in new_match:
             new_match.append(match_string)
             counter += 1
             last_match = sublist
-
-    if counter == 10:
-        break
 
 #print new_match
 for item in new_match:
